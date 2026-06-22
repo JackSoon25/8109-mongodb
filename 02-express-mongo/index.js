948647bd09394ea41e0c6fa147735bb3bdaa147a
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-06-22 08:22:21 +0000

    added search comments example

--- 02-express-mongo/index.js
+++ 02-express-mongo/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {connect} = require('./db');
+const { connect } = require('./db');
 
 require('dotenv').config();
 const mongoUri = process.env.MONGO_URI;
@@ -11,17 +11,18 @@ const app = express();
 app.use(express.json());
 
 // 3. route
-app.get('/health', function(req,res){
+app.get('/health', function (req, res) {
     res.json({
-        "message":"I'm alive!"
+        "message": "I'm alive!"
     })
 })
 
 async function main() {
 
     try {
-        const db = await connect(mongoUri, dbName );
-        app.get('/movies', async function(req,res){
+        const db = await connect(mongoUri, dbName);
+
+        app.get('/movies', async function (req, res) {
             try {
                 const movies = await db.collection("movies")
                     .find({})
@@ -38,18 +39,77 @@ async function main() {
             } catch (error) {
                 console.error(error);
                 res.json({
-                    'error':error
+                    'error': error
                 })
             }
         });
 
+        // example calls: /theaters/IN  or /theaters/TX or /theaters/PA
+        app.get('/theaters/:state', async function (req, res) {
+            try {
+                const state = req.params.state;
+                const theatres = await db.collection('theaters')
+                    .find({
+                        "location.address.state": state
+                    })
+                    .project({
+                        "location.address": 1
+                    })
+                    .toArray();
+
+                res.json({
+                    theatres: theatres
+                })
+
+            } catch (e) {
+                console.error(e);
+                res.status(500).json({
+                    "error": e
+                })
+            }
+        })
+
+        // query strings:
+        // ?date=1990-01-01&email=asd@asd.com 
+        // this will become in object in req.query:
+        // {
+        //    "date":"1990-01-01",
+        //    "email":"asd@asd.com"
+        // }
+        app.get('/comments', async function (req, res) {
+            const startDate = req.query.startDate;
+            const endDate = req.query.endDate;
+            const email = req.query.email;
+
+            try {
+
+            const comments = await db.collection('comments')
+                                .find({
+                                    date: {
+                                        $gte: new Date(startDate),
+                                        $lte: new Date(endDate)
+                                    },
+                                    email: email
+                                }).toArray();
+            res.json({
+                comments: comments
+            })
+
+            } catch (e) {
+                console.error(e);
+                res.status(500).json({
+                    'error': e
+                })
+            }
+        })
+
     } catch (error) {
-        console.error(error);
+        throw (error);
     }
 
 }
 main();
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started");
 })

3948dcf28eb58c6f20f4c6c097ab87dc4b46bd2e
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-06-23 05:38:30 +0000

    Added c, u and d routes

--- 03-recipes/index.js
+++ 03-recipes/index.js
@@ -1,35 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('./db');
+const { ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
+
+// use JSON requests (prevent req.body is undefined)
+app.use(express.json());
 
 async function main() {
 
     const db = await connect(process.env.MONGO_URI, "8109_recipes");
 
     // routes will in here
-    
+
     // Search for recipes
     // the req.query can contain following parameters:
     // name: string pattern for the name
     // tags: a comma delimited string list, eg. "easy,spicy"
-    app.get("/api/recipes", async function(req,res){
+    app.get("/api/recipes", async function (req, res) {
 
         const criteria = {};
 
         if (req.query.name) {
             criteria.name = {
                 $regex: req.query.name,
-                $options:"i"
+                $options: "i"
             }
         }
 
         if (req.query.cuisine) {
             criteria["cuisine.name"] = {
                 $regex: req.query.cuisine,
-                $options:"i"
+                $options: "i"
             }
         }
 
@@ -62,9 +66,81 @@ async function main() {
         })
     })
 
+    app.post("/api/recipes", async function (req, res) {
+        // get the new recipe from the request's body
+        const newRecipe = req.body;
+
+        // find the cuisine and assoicate the recipe with the cuisine
+        const cuisine = await db.collection("cuisines").findOne({
+            name: newRecipe.cuisine
+        })
+
+        const tags = await db.collection("tags").find({
+            name: {
+                $in: req.body.tags
+            }
+        }).toArray();
+
+        // replace the newRecipe's cuisine and tags with the ones from the database
+        newRecipe.cuisine = cuisine;
+        newRecipe.tags = tags;
+
+        const response = await db.collection("recipes").insertOne(newRecipe);
+        res.json({
+            message: "Successfully created recipe",
+            recipeId: response.insertedId
+        })
+    })
+
+    app.delete("/api/recipes/:recipeId", async function (req, res) {
+        const recipeId = req.params.recipeId;
+
+        // delete one document which _id is the same ObjectId(<given id>)
+        const result = await db.collection("recipes").deleteOne({
+            _id: new ObjectId(recipeId)
+        });
+        res.json({
+            "message": "The recipe has been deleted"
+        })
+    })
+
+    // PATCH or PUT
+    app.put("/api/recipes/:recipeId", async function (req, res) {
+        // get the new recipe from the request's body
+        const newRecipe = req.body;
+
+        // find the cuisine and assoicate the recipe with the cuisine
+        const cuisine = await db.collection("cuisines").findOne({
+            name: newRecipe.cuisine
+        })
+
+        const tags = await db.collection("tags").find({
+            name: {
+                $in: req.body.tags
+            }
+        }).toArray();
+
+        // replace the newRecipe's cuisine and tags with the ones from the database
+        newRecipe.cuisine = cuisine;
+        newRecipe.tags = tags;
+
+        const response = await db.collection("recipes").updateOne({
+            _id: new ObjectId(req.params.recipeId)
+        }, {
+            $set: newRecipe
+        });
+
+        res.json({
+            message: "Successfully updated recipe",
+
+        })
+    })
+
+
+
 }
 main();
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started");
 })
